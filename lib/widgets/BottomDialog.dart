fc1603ac16ed61b3f3444bfe79cebb82cfa6b8f3
0x5d0 <49813134+egor-baranov@users.noreply.github.com>
2021-12-09 22:06:08 +0300

    fix: bottom dialog corners and theming

--- lib/widgets/BottomDialog.dart
+++ lib/widgets/BottomDialog.dart
@@ -1,36 +1,44 @@
 import 'dart:ui';
 
 import 'package:flutter/material.dart';
-import 'package:modal_bottom_sheet/modal_bottom_sheet.dart';
 
 class BottomDialog {
-  static void show(BuildContext context,
-      {Widget? title, Widget? body}) {
+  static void show(BuildContext context, {Widget? title, Widget? body}) {
     showModalBottomSheet(
-      backgroundColor: Theme
-          .of(context)
-          .backgroundColor,
+      elevation: 0.5,
       shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.circular(32),
+        borderRadius: BorderRadius.only(
+          topRight: Radius.circular(32),
+          topLeft: Radius.circular(32),
+        ),
       ),
       isScrollControlled: true,
       context: context,
-      builder: (context) => Wrap(
-        children: [
-          Padding(
-            padding: EdgeInsets.all(8),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Center(
-                  child: Container(
-                    height: 8,
-                    width: 56,
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.all(Radius.circular(16.0)),
-                      color: Theme
-                              .of(context)
-                              .primaryColorLight,
+      builder: (context) {
+        return Wrap(
+          children: [
+            Container(
+              decoration: BoxDecoration(
+                color: Theme.of(context).backgroundColor,
+                borderRadius: BorderRadius.only(
+                  topRight: Radius.circular(32),
+                  topLeft: Radius.circular(32),
+                ),
+              ),
+              child: Padding(
+                padding: EdgeInsets.all(8),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Center(
+                      child: Container(
+                        height: 8,
+                        width: 56,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.all(Radius.circular(16.0)),
+                          color: Theme.of(context).primaryColorLight,
+                        ),
+                      ),
                     ),
                     SizedBox(height: 16),
                     Padding(
@@ -40,15 +48,14 @@ class BottomDialog {
                         child: title ?? Spacer(),
                       ),
                     ),
-                    Expanded(child: Center(child: body)),
+                    Center(child: body),
                   ],
                 ),
-                Center(child: body),
-              ],
-            ),
-          )
-        ],
-      ),
+              ),
+            )
+          ],
+        );
+      },
     );
   }
 }
