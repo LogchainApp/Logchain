5f6eec0ed084e1e0a62b098763879f93f13b04e9
0x5d0 <49813134+egor-baranov@users.noreply.github.com>
2021-12-09 19:36:47 +0300

    feat: Color.avgWith extension

--- lib/utils/extensions.dart
+++ lib/utils/extensions.dart
@@ -1,19 +1,11 @@
 import 'dart:ui';
 
-typedef Comparator<T> = int Function(T, T);
-
 extension ListExtension<T> on List<T> {
   List<T> copy() => this.toList();
 
   List<T> shuffled() {
     var it = this.copy();
     it.shuffle();
-    return it;
-  }
-
-  List<T> sorted(Comparator comparator) {
-    var it = this.copy();
-    it.sort(comparator);
     return it;
   }
 }
@@ -26,29 +18,5 @@ extension ColorExtension on Color {
       (this.green + other.green) ~/ 2,
       (this.blue + other.blue) ~/ 2,
     );
-  }
-}
-
-extension StringExtension on String {
-  bool isSubsequence(String other) {
-    if (this.length == 0) {
-      return true;
-    }
-
-    if (other.length == 0) {
-      return false;
-    }
-
-    var result = "";
-    for (final char in other.runes) {
-      if (String.fromCharCode(char) == this[result.length]) {
-        result += String.fromCharCode(char);
-      }
-
-      if (result == this) {
-        return true;
-      }
-    }
-    return false;
   }
 }
