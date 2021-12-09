2793a9b76cbc7ef76d36db8dc69a272b213924a5
0x5d0 <49813134+egor-baranov@users.noreply.github.com>
2021-12-09 22:06:08 +0300

    fix: bottom dialog corners and theming

--- lib/widgets/BottomDialog.dart
+++ lib/widgets/BottomDialog.dart
@@ -1,21 +1,30 @@
 import 'dart:ui';
 
 import 'package:flutter/material.dart';
-import 'package:modal_bottom_sheet/modal_bottom_sheet.dart';
 
 class BottomDialog {
-  static void show(BuildContext context,
-      {Widget? title, Widget? body, double height = 0.5}) {
-    showMaterialModalBottomSheet(
-      backgroundColor: Theme
-          .of(context)
-          .backgroundColor,
-      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(32)),
+  static void show(BuildContext context, {Widget? title, Widget? body}) {
+    showModalBottomSheet(
+      elevation: 0.5,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.only(
+          topRight: Radius.circular(32),
+          topLeft: Radius.circular(32),
+        ),
+      ),
+      isScrollControlled: true,
       context: context,
-      builder: (context) =>
-          Container(
-            child: FractionallySizedBox(
-              heightFactor: height,
+      builder: (context) {
+        return Wrap(
+          children: [
+            Container(
+              decoration: BoxDecoration(
+                color: Theme.of(context).backgroundColor,
+                borderRadius: BorderRadius.only(
+                  topRight: Radius.circular(32),
+                  topLeft: Radius.circular(32),
+                ),
+              ),
               child: Padding(
                 padding: EdgeInsets.all(8),
                 child: Column(
@@ -27,9 +36,7 @@ class BottomDialog {
                         width: 56,
                         decoration: BoxDecoration(
                           borderRadius: BorderRadius.all(Radius.circular(16.0)),
-                          color: Theme
-                              .of(context)
-                              .primaryColorLight,
+                          color: Theme.of(context).primaryColorLight,
                         ),
                       ),
                     ),
@@ -41,12 +48,14 @@ class BottomDialog {
                         child: title ?? Spacer(),
                       ),
                     ),
-                    Expanded(child: Center(child: body)),
+                    Center(child: body),
                   ],
                 ),
               ),
-            ),
-          ),
+            )
+          ],
+        );
+      },
     );
   }
 }
