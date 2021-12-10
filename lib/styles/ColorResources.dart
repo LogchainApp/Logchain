e66814bd0f65ecc9b09623f21ae78d05f081b74f
0x5d0 <49813134+egor-baranov@users.noreply.github.com>
2021-12-10 10:24:24 +0300

    fix: green color

--- lib/styles/ColorResources.dart
+++ lib/styles/ColorResources.dart
@@ -4,7 +4,7 @@ import 'dart:ui';
 class ColorResources {
   static const yellow = Color(0xFFFFE600);
   static const red = Color(0xFFFF4848);
-  static const green = Color(0xFF8DF374);
+  static const green = Color(0xFF37CE02);
 
   static const black = Color(0xFF212121);
   static const darkGrey = Color(0xFF787878);
