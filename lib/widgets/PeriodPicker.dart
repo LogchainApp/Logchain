ef3c52c54de2856e6d7fbfa414c0fc00a5580041
0x5d0 <49813134+egor-baranov@users.noreply.github.com>
2021-12-11 10:26:40 +0300

    feat: data update from net

--- lib/widgets/PeriodPicker.dart
+++ lib/widgets/PeriodPicker.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
-import 'package:logchain/models/Period.dart';
+import 'package:logchain/models/period.dart';
 import 'package:logchain/models/PeriodType.dart';
 import 'package:logchain/styles/TextStyles.dart';
 
