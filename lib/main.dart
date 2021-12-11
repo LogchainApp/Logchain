d8690c3f9a9dacba456deeaba145bf1d85061dc9
0x5d0 <49813134+egor-baranov@users.noreply.github.com>
2021-12-11 11:45:38 +0300

    fix: bottom dialog bugs

--- lib/main.dart
+++ lib/main.dart
@@ -118,6 +118,7 @@ class _MainPageState extends State<MainPage> {
                         "${currency.name} (${currency.symbol})",
                       ),
                       body: CryptoPage(currency: currency),
+                      height: 0.8,
                     );
                   },
                 ),
