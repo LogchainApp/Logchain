51062bee6288755235716c8bf06befc7a7be0eea
0x5d0 <49813134+egor-baranov@users.noreply.github.com>
2021-12-12 20:22:25 +0300

    fix: fixed search screen overscroll bug

--- lib/widgets/ui_components/CustomAppBar.dart
+++ lib/widgets/ui_components/CustomAppBar.dart
@@ -54,6 +54,7 @@ class CustomAppBar extends StatelessWidget {
                           "${currency.name} (${currency.symbol})",
                         ),
                         body: CryptoPage(currency: currency),
+                        height: 0.6,
                       );
                     },
                   ),
@@ -69,8 +70,12 @@ class CustomAppBar extends StatelessWidget {
                     color: Theme.of(context).primaryColorDark,
                   ),
                   onPressed: () {
-                    BottomDialog.show(context,
-                        title: Text("Menu"), body: Menu(), height: 0.55);
+                    BottomDialog.show(
+                      context,
+                      title: Text("Menu"),
+                      body: Menu(),
+                      height: 0.55,
+                    );
                   },
                 ),
               ),
