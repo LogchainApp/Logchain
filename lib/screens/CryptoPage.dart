2efa391fe0a6d16516482ef66ef747df59019ea9
0x5d0 <49813134+egor-baranov@users.noreply.github.com>
2021-12-15 17:29:58 +0300

    feat: pull to refresh + vibration on long tap or p2r

--- lib/screens/CryptoPage.dart
+++ lib/screens/CryptoPage.dart
@@ -7,6 +7,7 @@ import 'package:logchain/models/period.dart';
 import 'package:logchain/screens/search_screen.dart';
 import 'package:logchain/styles/ColorResources.dart';
 import 'package:logchain/utils/page_routes/fade_page_route.dart';
+import 'package:flutter_vibrate/flutter_vibrate.dart';
 import 'package:logchain/widgets/MenuButton.dart';
 import 'package:logchain/widgets/ui_components/BottomDialog.dart';
 import 'package:logchain/widgets/ui_components/PeriodPicker.dart';
@@ -14,6 +15,7 @@ import 'package:cached_network_image/cached_network_image.dart';
 import 'package:skeletons/skeletons.dart';
 import 'package:interactive_chart/interactive_chart.dart';
 import 'package:share_plus/share_plus.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 import '../network/network_provider.dart';
 import '../providers/UserDataProvider.dart';
@@ -70,202 +72,231 @@ class _CryptoPageState extends State<CryptoPage> {
         " (${(widget.currency.change / widget.currency.price * 100).toStringAsFixed(2)}%)";
     return Container(
       height: 596,
-      child: SingleChildScrollView(
-        physics: BouncingScrollPhysics(),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Container(
-                height: 80,
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  children: [
-                    CachedNetworkImage(
-                      width: 64,
-                      height: 64,
-                      imageBuilder: (context, imageProvider) =>
-                          Image(image: imageProvider),
-                      imageUrl: widget.currency.pictureLink,
-                      placeholder: (context, url) => SkeletonAvatar(
-                        style: SkeletonAvatarStyle(
-                          shape: BoxShape.circle,
-                          width: 64,
-                          height: 64,
+      child: RefreshIndicator(
+        onRefresh: () async {
+          Vibrate.feedback(FeedbackType.medium);
+          NetworkProvider.instance.fetchPrices();
+        },
+        color: Theme.of(context).shadowColor,
+        backgroundColor: Theme.of(context).canvasColor,
+        child: SingleChildScrollView(
+          physics: BouncingScrollPhysics(),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Container(
+                  height: 80,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      CachedNetworkImage(
+                        width: 64,
+                        height: 64,
+                        imageBuilder: (context, imageProvider) =>
+                            Image(image: imageProvider),
+                        imageUrl: widget.currency.pictureLink,
+                        placeholder: (context, url) => SkeletonAvatar(
+                          style: SkeletonAvatarStyle(
+                            shape: BoxShape.circle,
+                            width: 64,
+                            height: 64,
+                          ),
+                        ),
+                        errorWidget: (context, url, error) => SkeletonAvatar(
+                          style: SkeletonAvatarStyle(
+                            shape: BoxShape.circle,
+                            width: 64,
+                            height: 64,
+                          ),
                         ),
                       ),
-                      errorWidget: (context, url, error) => SkeletonAvatar(
-                        style: SkeletonAvatarStyle(
-                          shape: BoxShape.circle,
-                          width: 64,
-                          height: 64,
-                        ),
-                      ),
-                    ),
-                    SizedBox(width: 24),
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Text(
-                          "Price",
-                          style: Theme.of(context).textTheme.bodyText2,
-                        ),
-                        Text(
-                          "Change",
-                          style: Theme.of(context).textTheme.bodyText2,
-                        ),
-                        Text(
-                          "Volume",
-                          style: Theme.of(context).textTheme.bodyText2,
-                        ),
-                      ],
-                    ),
-                    Expanded(
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.end,
+                      SizedBox(width: 24),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
                         children: [
-                          Column(
-                            crossAxisAlignment: CrossAxisAlignment.end,
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            children: [
-                              Text(
-                                "\$${widget.currency.price.toStringAsFixed(2)}",
-                                style: Theme.of(context)
-                                    .textTheme
-                                    .bodyText2!
-                                    .copyWith(
-                                      color: Theme.of(context).primaryColorDark,
-                                      fontWeight: FontWeight.w600,
-                                    ),
-                              ),
-                              Text(
-                                change,
-                                style: Theme.of(context)
-                                    .textTheme
-                                    .bodyText2!
-                                    .copyWith(
-                                      color: widget.currency.change >= 0
-                                          ? ColorResources.green
-                                          : ColorResources.red,
-                                    ),
-                              ),
-                              Text(
-                                "2.60B",
-                                style: Theme.of(context)
-                                    .textTheme
-                                    .bodyText2!
-                                    .copyWith(
-                                      color: Theme.of(context).primaryColorDark,
-                                      fontWeight: FontWeight.w600,
-                                    ),
-                              ),
-                            ],
+                          Text(
+                            "Price",
+                            style: Theme.of(context).textTheme.bodyText2,
+                          ),
+                          Text(
+                            "Change",
+                            style: Theme.of(context).textTheme.bodyText2,
+                          ),
+                          Text(
+                            "Volume",
+                            style: Theme.of(context).textTheme.bodyText2,
                           ),
                         ],
                       ),
-                    ),
-                  ],
+                      Expanded(
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.end,
+                          children: [
+                            Column(
+                              crossAxisAlignment: CrossAxisAlignment.end,
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                Text(
+                                  "\$${widget.currency.price.toStringAsFixed(2)}",
+                                  style: Theme.of(context)
+                                      .textTheme
+                                      .bodyText2!
+                                      .copyWith(
+                                        color:
+                                            Theme.of(context).primaryColorDark,
+                                        fontWeight: FontWeight.w600,
+                                      ),
+                                ),
+                                Text(
+                                  change,
+                                  style: Theme.of(context)
+                                      .textTheme
+                                      .bodyText2!
+                                      .copyWith(
+                                        color: widget.currency.change >= 0
+                                            ? ColorResources.green
+                                            : ColorResources.red,
+                                      ),
+                                ),
+                                Text(
+                                  "2.60B",
+                                  style: Theme.of(context)
+                                      .textTheme
+                                      .bodyText2!
+                                      .copyWith(
+                                        color:
+                                            Theme.of(context).primaryColorDark,
+                                        fontWeight: FontWeight.w600,
+                                      ),
+                                ),
+                              ],
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
                 ),
               ),
-            ),
-            Divider(height: 16),
-            Container(
-              height: 64,
-              child: PeriodPicker(
-                periodType: widget.periodType,
-                onPeriodChanged: (period) {
+              Divider(height: 16),
+              Container(
+                height: 64,
+                child: PeriodPicker(
+                  periodType: widget.periodType,
+                  onPeriodChanged: (period) {
+                    setState(() {
+                      widget.periodType = period.periodType;
+                    });
+                  },
+                ),
+              ),
+              Divider(height: 16),
+              SizedBox(
+                height: 256,
+                child: FutureBuilder<List<CandleData>>(
+                  future: NetworkProvider.instance.fetchMarketChart(
+                    cryptoCurrency: widget.currency,
+                    currency: Currency.usd,
+                    days: Period.from(widget.periodType).days,
+                  ),
+                  builder: (context, snapshot) {
+                    if (snapshot.hasData) {
+                      print(snapshot.data!.length);
+                      try {
+                        return InteractiveChart(
+                          initialVisibleCandleCount: 64,
+                          candles: _computeTrendLines(snapshot.data!),
+                          onCandleResize: (zoomLevel) {},
+                        );
+                      } catch (Error) {
+                        return SkeletonAvatar(
+                          style: SkeletonAvatarStyle(
+                            shape: BoxShape.rectangle,
+                            height: 256,
+                            width: double.infinity,
+                          ),
+                        );
+                      }
+                    }
+
+                    return SkeletonAvatar(
+                      style: SkeletonAvatarStyle(
+                        shape: BoxShape.rectangle,
+                        height: 256,
+                        width: double.infinity,
+                      ),
+                    );
+                  },
+                ),
+              ),
+              Divider(height: 16),
+              MenuButton(
+                icon: UserDataProvider.instance.isFavourite(widget.currency)
+                    ? Icons.star_border_rounded
+                    : Icons.star_rounded,
+                title: UserDataProvider.instance.isFavourite(widget.currency)
+                    ? "Remove from favourites"
+                    : "Add to favourites",
+                onPressed: () {
                   setState(() {
-                    widget.periodType = period.periodType;
+                    UserDataProvider.instance.isFavourite(widget.currency)
+                        ? UserDataProvider.instance
+                            .removeFromFavourite(widget.currency)
+                        : UserDataProvider.instance
+                            .addToFavourite(widget.currency);
                   });
                 },
               ),
-            ),
-            Divider(height: 16),
-            SizedBox(
-              height: 256,
-              child: FutureBuilder<List<CandleData>>(
-                future: NetworkProvider.instance.fetchMarketChart(
-                  cryptoCurrency: widget.currency,
-                  currency: Currency.usd,
-                  days: Period.from(widget.periodType).days,
-                ),
-                builder: (context, snapshot) {
-                  if (snapshot.hasData) {
-                    print(snapshot.data!.length);
-                    return InteractiveChart(
-                      initialVisibleCandleCount: 64,
-                      candles: _computeTrendLines(snapshot.data!),
-                      onCandleResize: (zoomLevel) {},
-                    );
-                  }
-
-                  return SkeletonAvatar(
-                    style: SkeletonAvatarStyle(
-                      shape: BoxShape.rectangle,
-                      height: 256,
-                      width: double.infinity,
+              MenuButton(
+                icon: Icons.sync_alt,
+                title: "Compare",
+                onPressed: () {
+                  Navigator.of(context).push(
+                    FadePageRoute(
+                      SearchList(
+                        excludedCryptoCurrency: widget.currency,
+                        data: NetworkProvider.instance.fetchPrices(),
+                        hintText: "Compare ${widget.currency.symbol} with...",
+                        onItemTapCallback: (other) {
+                          BottomDialog.show(
+                            context,
+                            title: Text("Compare"),
+                            body: Compare(
+                              cryptoCurrencyLeft: widget.currency,
+                              cryptoCurrencyRight: other,
+                            ),
+                            height: 0.8,
+                          );
+                        },
+                      ),
+                      context: context,
                     ),
                   );
                 },
               ),
-            ),
-            MenuButton(
-              icon: UserDataProvider.instance.isFavourite(widget.currency)
-                  ? Icons.star_border_rounded
-                  : Icons.star_rounded,
-              title: UserDataProvider.instance.isFavourite(widget.currency)
-                  ? "Remove from favourites"
-                  : "Add to favourites",
-              onPressed: () {
-                setState(() {
-                  UserDataProvider.instance.isFavourite(widget.currency)
-                      ? UserDataProvider.instance
-                          .removeFromFavourite(widget.currency)
-                      : UserDataProvider.instance
-                          .addToFavourite(widget.currency);
-                });
-              },
-            ),
-            MenuButton(
-              icon: Icons.sync_alt,
-              title: "Compare",
-              onPressed: () {
-                Navigator.of(context).push(
-                  FadePageRoute(
-                    SearchList(
-                      excludedCryptoCurrency: widget.currency,
-                      data: NetworkProvider.instance.fetchPrices(),
-                      hintText: "Compare ${widget.currency.symbol} with...",
-                      onItemTapCallback: (other) {
-                        BottomDialog.show(
-                          context,
-                          title: Text("Compare"),
-                          body: Compare(
-                            cryptoCurrencyLeft: widget.currency,
-                            cryptoCurrencyRight: other,
-                          ),
-                          height: 0.8,
-                        );
-                      },
-                    ),
-                    context: context,
-                  ),
-                );
-              },
-            ),
-            MenuButton(
-              icon: Icons.ios_share_rounded,
-              title: "Share",
-              onPressed: () {
-                Share.share(
-                  'https://www.coingecko.com/en/coins/${widget.currency.id}',
-                );
-              },
-            ),
-            SizedBox(height: 16),
-          ],
+              if (widget.currency.website != "")
+                MenuButton(
+                  icon: Icons.link,
+                  title: "Website",
+                  onPressed: () async {
+                    await launch(widget.currency.website);
+                  },
+                ),
+              MenuButton(
+                icon: Icons.ios_share_rounded,
+                title: "Share",
+                onPressed: () {
+                  Share.share(
+                    'https://www.coingecko.com/en/coins/${widget.currency.id}',
+                  );
+                },
+              ),
+              SizedBox(height: 16),
+            ],
+          ),
         ),
       ),
     );
--- lib/screens/MainGrid.dart
+++ lib/screens/MainGrid.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:logchain/models/crypto_currency.dart';
 import 'package:logchain/network/network_provider.dart';
 import 'package:skeletons/skeletons.dart';
+import 'package:flutter_vibrate/flutter_vibrate.dart';
 
 import '../providers/UserDataProvider.dart';
 import '../utils/extensions.dart';
@@ -12,6 +13,8 @@ import '../widgets/CryptoCard.dart';
 typedef OnItemTapCallback = void Function(CryptoCurrency currency);
 typedef OnFavouriteTapCallback = void Function(CryptoCurrency currency);
 typedef OnLongPressCallback = void Function(CryptoCurrency currency);
+
+typedef Comparator<T> = int Function(T, T);
 
 class MainGrid extends StatelessWidget {
   final Future<Map<String, CryptoCurrency>> data;
@@ -23,14 +26,14 @@ class MainGrid extends StatelessWidget {
     required this.data,
     this.onItemTapCallback,
     this.onFavouriteTapCallback,
-    this.onLongPressCallback
+    this.onLongPressCallback,
   });
 
   @override
   Widget build(BuildContext context) {
     return DefaultTabController(
       initialIndex: 1,
-      length: 2,
+      length: 5,
       child: Scaffold(
         backgroundColor: Theme.of(context).backgroundColor,
         body: NestedScrollView(
@@ -53,6 +56,9 @@ class MainGrid extends StatelessWidget {
                   tabs: [
                     Text("Favourite"),
                     Text("Trending"),
+                    Text("Price"),
+                    Text("Volume"),
+                    Text("A-Z"),
                   ],
                   labelStyle: Theme.of(context).textTheme.headline1,
                   unselectedLabelStyle:
@@ -69,15 +75,28 @@ class MainGrid extends StatelessWidget {
             children: [
               buildGrid(
                 context,
-                UserDataProvider.instance.favourites.sorted(
-                  (a, b) => -a.changePercents.compareTo(b.changePercents),
-                ),
+                UserDataProvider.instance.favourites,
+                (a, b) => -a.changePercents.compareTo(b.changePercents),
               ),
               buildGrid(
                 context,
-                NetworkProvider.instance.currencyList.sorted(
-                  (a, b) => -a.changePercents.compareTo(b.changePercents),
-                ),
+                NetworkProvider.instance.currencyList,
+                (a, b) => -a.changePercents.compareTo(b.changePercents),
+              ),
+              buildGrid(
+                context,
+                NetworkProvider.instance.currencyList,
+                (a, b) => -a.price.compareTo(b.price),
+              ),
+              buildGrid(
+                context,
+                NetworkProvider.instance.currencyList,
+                (a, b) => -a.changePercents.compareTo(b.changePercents),
+              ),
+              buildGrid(
+                context,
+                NetworkProvider.instance.currencyList,
+                (a, b) => a.symbol.compareTo(b.symbol),
               ),
             ],
           ),
@@ -86,56 +105,69 @@ class MainGrid extends StatelessWidget {
     );
   }
 
-  Widget buildGrid(BuildContext context, List<CryptoCurrency> currencyList) {
-    return CustomScrollView(
-      physics: BouncingScrollPhysics(),
-      slivers: [
-        SliverPadding(
-          padding: EdgeInsets.all(16.0),
-          sliver: SliverGrid(
-            delegate: SliverChildBuilderDelegate(
-              (context, index) => FutureBuilder<Map<String, CryptoCurrency>>(
-                future: data,
-                builder: (context, snapshot) {
-                  if (snapshot.hasData) {
-                    print(snapshot.data!.keys);
-                    return AnimatedContainer(
-                      curve: Curves.easeInBack,
-                      duration: Duration(milliseconds: 600),
-                      child: CryptoCard(
-                        currency: snapshot.data!.values.firstWhere(
-                          (element) =>
-                              element.symbol == currencyList[index].symbol,
+  Widget buildGrid(
+    BuildContext context,
+    List<CryptoCurrency> currencyList,
+    Comparator<CryptoCurrency> comparator,
+  ) {
+    return RefreshIndicator(
+      color: Theme.of(context).shadowColor,
+      backgroundColor: Theme.of(context).canvasColor,
+      onRefresh: () async {
+        Vibrate.feedback(FeedbackType.medium);
+        NetworkProvider.instance.fetchPrices();
+      },
+      child: CustomScrollView(
+        physics: BouncingScrollPhysics(),
+        slivers: [
+          SliverPadding(
+            padding: EdgeInsets.all(16.0),
+            sliver: SliverGrid(
+              delegate: SliverChildBuilderDelegate(
+                (context, index) => FutureBuilder<Map<String, CryptoCurrency>>(
+                  future: data,
+                  builder: (context, snapshot) {
+                    if (snapshot.hasData) {
+                      var tmp = currencyList
+                          .map((e) => snapshot.data![e.symbol])
+                          .toList()
+                          .sorted((p0, p1) => comparator(p0, p1));
+                      print(snapshot.data!.keys);
+                      return AnimatedContainer(
+                        curve: Curves.easeInBack,
+                        duration: Duration(milliseconds: 600),
+                        child: CryptoCard(
+                          currency: tmp[index]!,
+                          onItemTapCallback: onItemTapCallback,
+                          onFavouriteTapCallback: onFavouriteTapCallback,
+                          onLongPressCallback: onLongPressCallback,
                         ),
-                        onItemTapCallback: onItemTapCallback,
-                        onFavouriteTapCallback: onFavouriteTapCallback,
-                        onLongPressCallback: onLongPressCallback,
+                      );
+                    }
+
+                    return SkeletonAvatar(
+                      style: SkeletonAvatarStyle(
+                        borderRadius: BorderRadius.circular(32),
+                        shape: BoxShape.rectangle,
+                        width: 96,
+                        height: 96,
                       ),
                     );
-                  }
-
-                  return SkeletonAvatar(
-                    style: SkeletonAvatarStyle(
-                      borderRadius: BorderRadius.circular(32),
-                      shape: BoxShape.rectangle,
-                      width: 96,
-                      height: 96,
-                    ),
-                  );
-                },
+                  },
+                ),
+                childCount: currencyList.length,
+                addAutomaticKeepAlives: true,
+                addSemanticIndexes: false,
               ),
-              childCount: currencyList.length,
-              addAutomaticKeepAlives: true,
-              addSemanticIndexes: false,
-            ),
-            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-              crossAxisCount: 2,
-              mainAxisSpacing: 24,
-              crossAxisSpacing: 24,
+              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 2,
+                mainAxisSpacing: 24,
+                crossAxisSpacing: 24,
+              ),
             ),
           ),
-        ),
-      ],
+        ],
+      ),
     );
   }
 }
